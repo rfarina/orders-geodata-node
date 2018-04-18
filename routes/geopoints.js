bcd08e1b5bfa911df37af187086d4203268f4f56
Rick Farina <rickrfarina@gmail.com>
2018-04-18 13:35:01 -0500

    access api g/w using request-promise

--- routes/geopoints.js
+++ routes/geopoints.js
@@ -1,28 +1,19 @@
-const aws = require('aws-sdk');
 const rp = require('request-promise');
 
 
 module.exports = function (app) {
     app.get('/api/geodata/:orderid/:timestamp', (req, res) => {
-        // res.json({
-        //     msg: 'You have reached the order tracking application',
-        //     orderid: req.params.orderid,
-        //     timestamp: req.params.timestamp
-        // });
-
-        // let geodata = {
-        //     'orderid': req.params.orderid,
-        //     'transtimestamp': req.params.timestamp
-        // };
 
         const geodata = {
-            "orderid": "12345",
-            "transtimestamp": "0"
+            'orderid': req.params.orderid,
+            'transtimestamp': req.params.timestamp
         };
 
+        // Access geographic location via API G/W
+        const uri = 'https://dde7i2es4g.execute-api.us-west-2.amazonaws.com/v1/api/orders-geodata-read';
         const options = {
             method: 'POST',
-            uri: 'https://dde7i2es4g.execute-api.us-west-2.amazonaws.com/v1/api/orders-geodata-read',
+            uri: uri,
             body: geodata,
             json: true,
             headers: {
@@ -33,46 +24,28 @@ module.exports = function (app) {
         };
 
         rp(options)
-            .then(function (data) {
+            .then(function (response) {
                 // POST succeeded...
-                let item = data.Items[0];
+                let item;
+                console.log(response);
+                if(response.data.Items) {
+                    item = response.data.Items[0];
+                } else {
+                    item = '';
+                }
 
                 res.json({
-                    data: item
+                    item: item
                 });
             })
             .catch(function (err) {
                 // POST failed...
+                console.log(err);
                 res.json({
                     msg: 'An error occurred',
                     err: err
                 });
             });
-
-
-        // $.ajax({
-        //     type: 'POST',
-        //     url: 'https://hxrwcabsx0.execute-api.us-west-2.amazonaws.com/v2/api/geodata-read',
-        //     data: JSON.stringify(geodata),
-        //     dataType: 'json',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-
-        // }).done(function (data, status) {
-        //     console.log('data: \n', data);
-        //     let item = data.Items[0];
-        //     let position = {
-        //         latitude: Number(item.lat.S),
-        //         longitude: Number(item.lon.S)
-        //     };
-
-        //     res.json({
-        //         latitude: position.latitude,
-        //         longitude: position.longitude
-        //     });
-        // });
-
 
     });
 };

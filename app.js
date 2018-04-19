d1fbeed89d29cdf7dd22928d64775202c7020d7d
Rick Farina <rickrfarina@gmail.com>
2018-04-19 01:16:08 -0500

    bootstrap, fontawesome

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Establish routes
-require('./routes/geopoints')(app);
-
 // Establish middleware for static files
 app.use(express.static('public'));
 
+// Establish routes
+require('./routes/geopoints.js')(app);
 
-
-// app.get('/api/geodata/:orderid/:timestamp', (req, res) => {
-//     res.json({
-//         msg: 'You have reached the order tracking application',
-//         orderid: req.params.orderid,
-//         timestamp: req.params.timestamp
-//     });
-
-
-// });
 
 
 let port = 5001;
--- routes/geopoints.js
+++ routes/geopoints.js
@@ -2,6 +2,13 @@ const rp = require('request-promise');
 
 
 module.exports = function (app) {
+    // Enable CORS
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+      
     app.get('/api/geodata/:orderid/:timestamp', (req, res) => {
 
         const geodata = {
